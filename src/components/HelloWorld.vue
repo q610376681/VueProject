7051d51d92a2dbb6dbc2e09798b236ee505f612c
pmtz <610376681@qq.com>
2019-01-06 10:39:09 +0800

    zhang first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="add-or-minus">
+    <button @click="minus">-1</button>
+    <input disabled :value="number">
+    <button @click="add">+1</button>
+    <div v-if="meetTen">已经到10了</div>
+    <div>{{number|formatText}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      number: 0
+    }
+  },
+  filters: {
+    formatText (value) {
+      return ['零', '一', '二']
+    }
+  },
+  computed: {
+    meetTen () {
+      return this.number === 10
+    }
+  },
+  methods: {
+    minus () {
+      this.number -= 1
+    },
+    add () {
+      this.number += 1
+    }
+  }
+}
+</script>
