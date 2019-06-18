f88f580ac3d135465dbd490be3c6696c5112bd45
zhangchen <zhangchen0419@qq.com>
2019-06-18 11:34:53 +0800

    消除警告

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,5 +1,5 @@
 var globalConfig = {
-  ip: 'http://202.112.88.15:8080/bnuSHBooks',
+  ip: 'http://localhost:8080/bnuSHBooks',
   imgIp: 'http://219.224.25.21',
   localWindowsimgIp: 'D:\\myCode\\demo\\VueProject\\src\\assets\\img'
 }
